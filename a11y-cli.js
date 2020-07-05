127bb54813f11fb63069477dc7463addb8802e6c
Sebastian Rodriguez <sebasrodriguez@gmail.com>
2020-07-05 13:56:52 -0300

    wip: Testing install

--- a11y-cli.js
+++ a11y-cli.js
@@ -1,17 +1,51 @@
 const core = require("@actions/core");
+const exec = require("@actions/exec");
+const fs = require("fs");
 const pa11y = require("pa11y");
+const quote = require("quote");
 
-module.exports = ({ urls, failOnError }) => {
-  const logIssue = (issue) => {
-    core.debug(failOnError);
-    if (failOnError) {
-      core.setFailed(issue.message);
-    } else {
-      core.warning(issue.message);
-    }
-  };
+const useYarn = () => fs.existsSync(yarnFilename);
 
+const install = () => {
+  // prevent lots of progress messages during install
+  core.exportVariable("CI", "1");
+
+  if (useYarn()) {
+    core.debug("installing NPM dependencies using Yarn");
+    return io.which("yarn", true).then((yarnPath) => {
+      core.debug(`yarn at "${yarnPath}"`);
+      return exec.exec(quote(yarnPath), ["--frozen-lockfile"]);
+    });
+  } else {
+    core.debug("installing NPM dependencies");
+    return io.which("npm", true).then((npmPath) => {
+      core.debug(`npm at "${npmPath}"`);
+      return exec.exec(quote(npmPath));
+    });
+  }
+};
+
+const installMaybe = (shouldInstall) => {
+  if (!shouldInstall) {
+    return Promise.resolve();
+  }
+
+  return install();
+};
+
+const logIssue = (issue, failOnError) => {
+  core.debug(failOnError);
+  if (failOnError) {
+    core.setFailed(issue.message);
+  } else {
+    core.warning(issue.message);
+  }
+};
+
+module.exports = async ({ urls, failOnError, install, startCommand }) => {
   try {
+    await installMaybe(install);
+
     urls.forEach(async (url) => {
       const { pageUrl, issues } = await pa11y(url, {
         chromeLaunchConfig: {
@@ -24,7 +58,7 @@ module.exports = ({ urls, failOnError }) => {
       }
 
       issues.forEach((issue) => {
-        logIssue(issue);
+        logIssue(issue, failOnError);
       });
 
       if (issues.length) {
--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const core = require("@actions/core");
 
 const a11yCli = require("./a11y-cli");
 
+const getBooleanInput = (name) => core.getInput(name).toLowerCase() === "true";
+
 try {
   const urls = core.getInput("urls");
-  const failOnError = core.getInput("fail-on-error").toLowerCase() === "true";
+  const failOnError = getBooleanInput("fail-on-error");
+  const install = getBooleanInput("install");
+  const startCommand = core.getInput("start-command");
 
   a11yCli({
     urls: urls.split(","),
     failOnError,
+    install,
+    startCommand,
   });
 } catch (error) {
   core.setFailed(error.message);
